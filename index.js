29ef8c66489eb0edba065273388ca258e4506cfd
Your Name <shaheer838838@gmail.com>
2025-09-16 22:08:11 +0500

    refactor: comment out server listening code and simplify Vercel configuration

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import app from "./src/app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
 
 export default app;
