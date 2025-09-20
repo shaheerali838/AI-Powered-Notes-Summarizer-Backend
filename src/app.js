3018deac1aa0e544b11e1ec6731d938c99bdab92
Your Name <shaheer838838@gmail.com>
2025-09-20 22:43:43 +0500

    mmm

--- src/app.js
+++ src/app.js
@@ -97,8 +97,14 @@ app.use((req, res, next) => {
 // Handle preflight OPTIONS requests for all routes
 app.options("/*", (req, res) => {
   res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin, Cache-Control, X-File-Name");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With, Accept, Origin, Cache-Control, X-File-Name"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Max-Age", "86400");
   res.sendStatus(200);
